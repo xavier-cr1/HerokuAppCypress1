608d2320505d428946542d28ba36b4fa59d6b65e
Xavier Casafont <XCasafont@globalshares.com>
2022-02-17 17:26:54 +0100

    Added basic cy get request to express server

---
+++ cypress/integration/PersonServiceAPI/BasicPersonService.spec.js
@@ -0,0 +1,14 @@
+describe('Basic Person Service Tests', () => {
+    
+    it('Get people from express server', () => {
+      cy.request({
+          method: 'GET',
+          url: 'http://localhost:49160/people'
+          
+      }).then((res) => {
+          expect(res.status).to.eq(200)
+          expect(res.body).to.have.property('Names')
+          expect(res.body.Names[0]).to.eq('Tony')
+      })
+    })
+})
